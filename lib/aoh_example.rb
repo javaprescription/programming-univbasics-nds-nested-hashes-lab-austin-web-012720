3866b8d000646c630536069cdd0ba12a06e8c47f
Austin Scott <austinscott4@gmail.com>
2019-12-09 18:20:50 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,13 @@ end
 #Array. Ruby constants are denoted by ALL_CAPS
 
 def literal_aoh
-
+  array = [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G,
+    ]
 end
 # Using Array literal syntax only, build a nested array that uses the data in
 # held in the constants
